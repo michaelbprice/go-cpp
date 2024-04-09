ebfd43601c0122e712b8cc6c6c58a7d82ee1c4ad
Michael B. Price <michael.b.price.dev@gmail.com>
2024-04-09 17:48:25 +0000

    Removing ImGui code for now while I figure out how to get a backend correctly hooked up.
    Having only an SDL window is sufficient for the now.

--- go-gui/source/main.cpp
+++ go-gui/source/main.cpp
@@ -1,10 +1,5 @@
-#include "imgui.h"
-#include "imgui_impl_sdl.h"
-#include "imgui_impl_opengl3.h"
-#include <SDL.h>
+#include "SDL2/SDL.h"
 
-#include <optional>
-#include <tuple>
 
 namespace /* private */ {
 
@@ -28,43 +23,21 @@ struct MyInit
             return;
         }
 
-        constexpr auto glsl_version = "#version 130";
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_FLAGS, 0);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 3);
-        SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 0);
+        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
+        window = SDL_CreateWindow("Dear ImGui SDL2 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
 
-        // Create window with graphics context
-        SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-        SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
-        SDL_GL_SetAttribute(SDL_GL_STENCIL_SIZE, 8);
-        SDL_WindowFlags window_flags = (SDL_WindowFlags)(SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE | SDL_WINDOW_ALLOW_HIGHDPI);
-        window = SDL_CreateWindow("Dear ImGui SDL2+OpenGL3 example", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, window_flags);
         if (window == nullptr)
         {
             init_error = true;
-            printf("Error: SDL_CreateWindow(): %s\n", SDL_GetError());
+            SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Error: SDL_CreateWindow(): %s\n", SDL_GetError());
             return;
         }
-        SDL_GLContext gl_context = SDL_GL_CreateContext(window);
-        SDL_GL_MakeCurrent(window, gl_context);
-        SDL_GL_SetSwapInterval(1); // Enable vsync
-
-        // Initialize ImGui
-        IMGUI_CHECKVERSION();
-        ImGui::CreateContext();
-
-        ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
-        ImGui_ImplOpenGL3_Init(glsl_version);
     }
 
     ~MyInit ()
     {
         if (window)
         {
-            ImGui_ImplOpenGL3_Shutdown();
-            ImGui_ImplSDL2_Shutdown();
-            ImGui::DestroyContext();
             window = nullptr;
         }
     }
@@ -72,44 +45,32 @@ struct MyInit
 
 }
 
-int main (int argc, char * argv[])
-{
+int main(int argc, char* argv[]) {
     MyInit init;
-    if (init.init_error)
-    {
-        return -1;
-    }
+    if (init.init_error) { return -1;}  
 
-    ImGuiIO& io = ImGui::GetIO();
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;
-
-    while (bool done = false)
+    bool keepRunning = true;
+    while (keepRunning)
     {
         SDL_Event event;
-        while (SDL_PollEvent(&event))
+        while (SDL_PollEvent(&event) > 0)
         {
-            ImGui_ImplSDL2_ProcessEvent(&event);
-            if (event.type == SDL_QUIT)
+            switch (event.type)
             {
-                done = true;
-            }
-            if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(init.window))
-            {
-                done = true;
+             case SDL_QUIT:
+                keepRunning = false;
+                break;
+
+             case SDL_WINDOWEVENT:
+                if (event.window.event == SDL_WINDOWEVENT_CLOSE && event.window.windowID == SDL_GetWindowID(init.window))
+                {
+                    keepRunning = false;
+                }
+                break;
+
+             default:
+                SDL_LogInfo(SDL_LOG_CATEGORY_APPLICATION, "Unhandled Event: %d", event.type);
             }
         }
-
-        // Start the Dear ImGui frame
-        ImGui_ImplOpenGL3_NewFrame();
-        ImGui_ImplSDL2_NewFrame();
-        ImGui::NewFrame();
-        ImGui::ShowDemoWindow();
-
-        // Logic
-
-        ImGui::Render();
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        SDL_GL_SwapWindow(init.window);
     }
 }
